9e48de3312ac4e886d986b736b3438f8a900ff99
Alondra Yamilet Olea Cardenas <158488326+AlondraOleaa@users.noreply.github.com>
2024-03-12 15:55:13 +0000

    Nodo-Pila

--- miPrincipal/Nodo.java
+++ miPrincipal/Nodo.java
@@ -1,4 +1,31 @@
 package miPrincipal;
 public class Nodo<T>{
-    
+    // Atributos de valor tipo T. Almacenan la referencia al objeto *(variable generica)*
+    //que se guarda en el nodo
+    private T valor;
+    //Referencia al siguiente nodo enlazado
+    Nodo<T> siguiente;
+
+    //Constructr por defecto
+    public Nodo(){
+        valor = null;
+        siguiente = null;
+    }
+    //Regresa el valor
+    public T getValor(){
+        return valor;
+    }
+    //Modifica el valor
+    public void setValor(T valor){
+        this.valor = valor;
+    }
+    //Devuelve el atributo siguiente
+    public Nodo<T> getSiguiente(){
+        return siguiente;
+    }
+    //Modifica el atributo siguiente
+    public void setSiguiente(Nodo<T> siguiente){
+        this.siguiente = siguiente;
+    }
+    //PILA UNA LISTA LIGADA
 }
---
+++ miPrincipal/Pila.java
@@ -0,0 +1,44 @@
+package miPrincipal;
+public class Pila<T> {
+
+    ///Atributo cabeza que apunta al tope de la pila
+    private Nodo<T> cabeza;
+    //Almacena el total de elementos de la pila
+    private int tamanio;
+
+    //Constructor
+    public Pila(){
+        cabeza = null;
+        tamanio = 0;
+    }
+
+    //Devuelve el total de elementos de la pila
+    public int getTamanio(){
+        return tamanio;
+    }
+    //Verifica si la pila esta vacia
+    public boolean esVacio(){
+        return (cabeza == null);
+    }
+    //Apila un elemento nuevo
+    public void apilar(T valor){
+        //Crear un nuevo Nodo
+        Nodo<T> nuevo = new Nodo<T>(); //CREAR NUEVO NODO
+        //Fijar el valor dentro del nodo
+        nuevo.setValor(valor);
+        if(esVacio()){
+            //Cuando esta vacia la cabeza apunta al nodo nuevo
+            cabeza = nuevo;
+        }else{
+            //Se enlaza el campo siguiente el nuevo con la cabeza
+            nuevo.setSiguiente(cabeza);
+            //La nueva cabeza de la pila pasa a ser nuevo
+            cabeza = nuevo;
+        }
+        //Incremento el tamaño de la pila por el nuevo nodo
+        tamanio ++;
+    } 
+
+
+    
+}
