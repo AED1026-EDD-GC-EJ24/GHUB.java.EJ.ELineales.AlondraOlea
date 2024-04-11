cbc3cb0f5ace61db940f0eb8f7d29314d186cd75
Alondra Yamilet Olea Cardenas <158488326+AlondraOleaa@users.noreply.github.com>
2024-04-11 15:56:29 +0000

    lista Terminada

--- miPrincipal/DemoListaSimple.java
+++ miPrincipal/DemoListaSimple.java
@@ -2,11 +2,16 @@ package miPrincipal;
 
 public class DemoListaSimple {
     public static void menu() {
+        try{
         System.out.println("===============================");
         System.out.println("     LISTA SIMPLE ENLAZADA     ");
         System.out.println("===============================");
         System.out.println( );
+
         
+        }catch(PosicionIlegalException e){
+            e.printStackTrace();
+        }
     }
     
 }
