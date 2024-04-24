1de0bbbae4bb8768db920324d37ef03998ff51df
Alondra Yamilet Olea Cardenas <158488326+AlondraOleaa@users.noreply.github.com>
2024-04-24 02:31:45 +0000

    nuevo

--- miPrincipal/DemoListaSimple.java
+++ miPrincipal/DemoListaSimple.java
@@ -8,7 +8,6 @@ public class DemoListaSimple {
         System.out.println( );
 
         Lista<Integer> lista = new Lista<Integer>();
-
         //Agrega tres elementos
         lista.agregar(12); //0
         lista.agregar(15); //1
@@ -28,17 +27,14 @@ public class DemoListaSimple {
         System.out.println("Dato en la posicion 2: "+lista.getValor(2));
         System.out.println("Dato en la posicion 3: "+lista.getValor(3));
         System.out.println("Dato en la posicion 4: "+lista.getValor(4));
-        //Insertar un elemento el la poscion 6;
-        //lista.insertar(17,6);
-        //Eliminar la posicion 0
         lista.remover(0);
         //Eliminar la posicion 3
         lista.remover(3);
         System.out.println("Muestro elementos después de la eliminacion");
         System.out.println("Dato en la posicion 0: "+lista.getValor(0));
         System.out.println("Dato en la posicion 1: "+lista.getValor(1));
-        System.out.println("Dato en la posicion 2: "+lista.getValor(2));
-        
+        System.out.println("Dato en la posicion 2: "+lista.getValor(2));     
     }
     
 }
+
